cecbd725969979257344464c14c6954f5dcdc3cf
laeb16ab <laeb16ab@student.cbs.dk>
2017-11-29 12:11:41 +0100

    user and admin quiz pages

---
+++ js/CoursesQuizAdmin.js
@@ -0,0 +1,37 @@
+$(document).ready(() => {
+
+    SDK.User.loadNav();
+
+    SDK.quiz.loadQuizzes((err, myQuizzes) => {
+        if (err) throw err;
+        var quizzes = JSON.parse(myQuizzes);
+        console.log(quizzes);
+
+        $("#tablehead").append("<thead>\n" +
+            "<th>Quiz Titel</th>\n" +
+            "<th><button class=\"quizCreateBtn btn-primary btn-lg\" >Create quiz</button></th>\n" +
+            "</thead>")
+
+        $.each(quizzes, function (i, val) {
+            var tr = '<tr>';
+            tr += '<td width="80%">' + quizzes[i].quizTitle + '</td>';
+            tr += '<td width="20%"><button class="quizDeleteBtn btn-primary btn-lg" data-key="' + (i+1) + '">Delete quiz</button></td>';
+            tr += '</tr>';
+            i + 1;
+            $("#quizList").append(tr);
+        });
+
+        $(".quizCreateBtn").click(function () {
+            console.log(this.id)
+            window.location.href = "newQuiz.html";
+
+        });
+
+
+
+    });
+
+
+
+
+});
